0cab46ac886b65373ff11b9518507fc009b3f42f
kevinj0695 <78401891+kevinj0695@users.noreply.github.com>
2023-04-09 12:24:59 +0900

    Update scroller.js

--- src/scroller.js
+++ src/scroller.js
@@ -5,13 +5,12 @@ export default class Scroll {
     this.scroller = document.querySelector('.scroller--var');
     this.docEl = document.documentElement;
     this.scrollHeight = this.docEl.scrollHeight - this.docEl.clientHeight;
-    this.scroller.style.width = (this.docEl.scrollTop/this.scrollHeight) * 100 + '%'
+    this.scroller.style.transform = `translateX(${-100 + (this.docEl.scrollTop/this.scrollHeight * 100)}%)`;
   }
 
   getPercent() {
     let scrollTop = this.docEl.scrollTop;
     let scrollPercent = (scrollTop/this.scrollHeight) * 100;
-    this.scroller.style.transfrom = `translateX(${-100 + scrollPercent}%)`;
-    return scrollPercent;
+    this.scroller.style.transform = `translateX(${-100 + scrollPercent}%)`;
   }
 }
